e5e69e4a0d74b5c70d2eb2c27e517c451266c282
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-10-25 15:12:30 +0200

    deps loading

--- src/HiGHS.jl
+++ src/HiGHS.jl
@@ -1,3 +1,12 @@
 module HiGHS
 
+if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+    include("../deps/deps.jl")
+else
+    error("HiGHS not properly installed. Please run import Pkg; Pkg.build(\"HiGHS\")")
+end
+
+include(joinpath("wrapper", "libhighs_api.jl"))
+include(joinpath("wrapper", "libhighs_common.jl"))
+
 end # module
