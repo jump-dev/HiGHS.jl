fa03c978d3eb187498da4e22f6d1ce9555ce62ab
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-12-02 18:01:06 +0100

    fix build

--- deps/build.jl
+++ deps/build.jl
@@ -8,12 +8,12 @@ const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__
 const products = Product[LibraryProduct(prefix, "libhighs", :libhighs)]
 
 # Download binaries from hosted location
-bin_prefix = "https://github.com/matbesancon/HiGHSBuilder/releases/download/v0.1.0"
+const bin_prefix = "https://github.com/matbesancon/HiGHSBuilder/releases/download/v0.1.1"
 
-download_info = Dict(
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc4))  => ("$bin_prefix/highs.v0.1.0.x86_64-linux-gnu-gcc4.tar.gz", "a0df6e2899f2ba5aadfbd5d865136abf31e41f768156038bca72f859dbed5373"),
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7))  => ("$bin_prefix/highs.v0.1.0.x86_64-linux-gnu-gcc7.tar.gz", "9b2d37d473126f3394a131e718f22114a892effc0e23c7471a3a242c6122a539"),
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8))  => ("$bin_prefix/highs.v0.1.0.x86_64-linux-gnu-gcc8.tar.gz", "277ddbc459941a1cd4ce052c16008ccc65c379cdee34377eda70f1ecfe1d48ac"),
+const download_info = Dict(
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc4))  => ("$bin_prefix/highs.v0.1.1.x86_64-linux-gnu-gcc4.tar.gz", "efc83bcd2052b46b8148a866fcafb88d161f0a0cd5735090586f220e62c28035"),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7))  => ("$bin_prefix/highs.v0.1.1.x86_64-linux-gnu-gcc7.tar.gz", "6b3f2f2d1405e3f73ed0702607966f9b86fb2e0b0edee1cae6f9b9c9552e09eb"),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8))  => ("$bin_prefix/highs.v0.1.1.x86_64-linux-gnu-gcc8.tar.gz", "97f275fb5dbe729fd29c3d74b9a3c6cc572a8393345943328f3a5de8a267c057"),
 )
 
 # First, check to see if we're all satisfied
