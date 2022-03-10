c02745f4e74aec304a6c834352bb46d58b8a5530
odow <o.dowson@gmail.com>
2022-03-10 16:31:37 +1300

    Update

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,10 @@ using HiGHS_jll
 @show run(`valgrind ls`)
 
 exe = HiGHS_jll.highs_path
-@show run(`valgrind --leak-check=full $(exe) fail-on-32-bit.mps`)
+run(`$(exe) --parallel=off --presolve=off fail-on-32-bit.mps`)
+run(`$(exe) --parallel=off fail-on-32-bit.mps`)
+run(`$(exe) fail-on-32-bit.mps`)
+@show run(`valgrind --leak-check=full $(exe) --parallel=off --presolve=off fail-on-32-bit.mps`)
 # @show run(`export VALGRIND_LIB="/usr/local/lib/valgrind"; valgrind --leak-check=full $(exe) fail-on-32-bit.mps`)
 
 # valgrind = addenv(`valgrind`, HiGHS_jll.JLLWrappers.JLLWrappers.LIBPATH_env=>HiGHS_jll.LIBPATH[]);
