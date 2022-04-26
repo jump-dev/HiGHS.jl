be4ba737d8eb2a78226198f6e0b4a1e7767802c0
odow <o.dowson@gmail.com>
2022-04-26 15:26:38 +1200

    Add license headers

--- src/HiGHS.jl
+++ src/HiGHS.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2019 Mathieu Besançon, Oscar Dowson, and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 module HiGHS
 
 import HiGHS_jll
