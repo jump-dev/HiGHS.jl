8580d0e401a2b6c856c94a7b96aad2e91b1ddeb7
odow <o.dowson@gmail.com>
2025-06-06 15:39:37 +1200

    Add gen/prologue.jl to simplify rebuilding C API

---
+++ gen/prologue.jl
@@ -0,0 +1,7 @@
+# Copyright (c) 2019 Mathieu Besançon, Oscar Dowson, and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+# Disable JuliaFormatter for this file.
+#!format:off
